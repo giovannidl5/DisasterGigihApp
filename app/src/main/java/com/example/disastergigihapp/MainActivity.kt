fec0e12ad2ee3553745493011d6585c5edcae659
Giovanni De Leandro <giovannideleandro@gmail.com>
2023-07-24 15:35:47 +0700

    chore: add view binding

--- app/src/main/java/com/example/disastergigihapp/MainActivity.kt
+++ app/src/main/java/com/example/disastergigihapp/MainActivity.kt
@@ -2,17 +2,20 @@ package com.example.disastergigihapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.disastergigihapp.databinding.ActivityMainBinding
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
 
 var mapView: MapView? = null
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        mapView = findViewById(R.id.mapView)
+        mapView = binding.mapView
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
     }
 }
